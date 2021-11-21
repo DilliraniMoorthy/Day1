c677293356a74775994a952543eec8baf6fffb3a
DilliraniMoorthy <ranjumoorthy1318@gmail.com>
2021-11-21 12:02:22 +0530

    task

---
+++ src/org/test/While.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class While {
+	
+	public static void main(String[] args) {
+		int a=10;
+		
+		while(a<=20) {
+			System.out.println(a);
+			a++;
+		
+	}
+
+	
+	}
+	
+}
